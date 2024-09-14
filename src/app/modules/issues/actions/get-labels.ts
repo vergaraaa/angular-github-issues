4c75c97174ae8d55caa4ad9e8235412b1f4a9620
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-14 17:46:13 +0200

    ✨ actions and helpers, barrel files

---
+++ src/app/modules/issues/actions/get-labels.ts
@@ -0,0 +1,19 @@
+import { sleep } from '@helpers/sleep';
+import { GithubLabel } from '../interfaces/github-label.interface';
+
+export const getLabels = async (): Promise<GithubLabel[]> => {
+  await sleep(1500);
+
+  try {
+    const resp = await fetch(
+      `https://api.github.com/repos/angular/angular/labels`
+    );
+    if (!resp.ok) throw "Cant't load labels";
+
+    const labels: GithubLabel[] = await resp.json();
+
+    return labels;
+  } catch (error) {
+    throw "Cant't load labels";
+  }
+};
---
+++ src/app/modules/issues/interfaces/index.ts
@@ -0,0 +1,1 @@
+export * from './github-label.interface';
