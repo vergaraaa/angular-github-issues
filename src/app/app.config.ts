74010b1d96cf2af34603c843664b31f5d814205c
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-14 17:30:21 +0200

    ✨ install tanstack query

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,15 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {
+  provideAngularQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), ɵprovideZonelessChangeDetection()],
+  providers: [
+    provideRouter(routes),
+    ɵprovideZonelessChangeDetection(),
+    provideAngularQuery(new QueryClient()),
+  ],
 };
