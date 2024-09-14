80367e153bd8cf056ee290b057960550b018b858
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-14 17:19:30 +0200

    ✨ create pages and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'issues',
+    loadComponent: () =>
+      import('./modules/issues/pages/issues-list/issues-list-page.component'),
+  },
+  {
+    path: 'issues/:number',
+    loadComponent: () =>
+      import('./modules/issues/pages/issue/issue-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'issues',
+  },
+];
---
+++ src/app/modules/issues/pages/issue/issue-page.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issue-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './issue-page.component.html',
+})
+export default class IssuePageComponent {}
