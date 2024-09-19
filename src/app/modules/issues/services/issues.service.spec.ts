e3e00f1cd109d1230871bfe55bae611d619c54ea
Edgar Vergara <edgarvm13@hotmail.com>
2024-09-19 18:11:57 +0200

    ✅ issues service tests

--- src/app/modules/issues/services/issues.service.spec.ts
+++ src/app/modules/issues/services/issues.service.spec.ts
@@ -4,6 +4,7 @@ import {
   provideAngularQuery,
   QueryClient,
 } from '@tanstack/angular-query-experimental';
+import { State } from '../interfaces';
 
 describe('IssuesService', () => {
   let service: IssuesService;
@@ -35,5 +36,44 @@ describe('IssuesService', () => {
     expect(typeof label.name).toBe('string');
     expect(typeof label.node_id).toBe('string');
     expect(typeof label.url).toBe('string');
+  });
+
+  it('should set selected state, OPEN, CLOSED, ALL', async () => {
+    service.showIssuesByState(State.Closed);
+    expect(service.selectedState()).toBe(State.Closed);
+    const { data } = await service.getIssuesQuery.refetch();
+
+    data?.forEach((issue) => {
+      expect(issue.state).toBe(State.Closed);
+    });
+
+    service.showIssuesByState(State.Open);
+    expect(service.selectedState()).toBe(State.Open);
+    const { data: dataOpen } = await service.getIssuesQuery.refetch();
+
+    dataOpen?.forEach((issue) => {
+      expect(issue.state).toBe(State.Open);
+    });
+  });
+
+  it('should set selectedLabels', async () => {
+    service.toggleLabel('Accessibility');
+    expect(service.selectedLabels().has('Accessibility')).toBeTrue();
+
+    service.toggleLabel('Accessibility');
+    expect(service.selectedLabels().has('Accessibility')).toBeFalse();
+  });
+
+  it('should set selectedLabels', async () => {
+    const label = 'Accessibility';
+    service.toggleLabel(label);
+    expect(service.selectedLabels().has(label)).toBeTrue();
+
+    const { data } = await service.getIssuesQuery.refetch();
+
+    data?.forEach((issue) => {
+      const hasLabel = issue.labels.some((l) => l.name === label);
+      expect(hasLabel).toBeTrue;
+    });
   });
 });
